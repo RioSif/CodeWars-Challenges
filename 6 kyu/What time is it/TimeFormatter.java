b8451a5bdaf4b118e11fae0f5cc9bc903cde3301
RioSif <riosif@gmail.com>
2021-04-21 12:57:16 +0300

    :memo:

--- 6 kyu/What time is it/TimeFormatter.java
+++ 6 kyu/What time is it/TimeFormatter.java
@@ -4,23 +4,56 @@ public class TimeFormatter {
 
     public static String toHumanTime(String time) {
         // TODO: Code me!
-        String[] hoursNumberToWord = {"zero", "one", "two", "three", "four", "five",
-                "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
+        String[] hoursNumberToWord = {"twelve", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten", "eleven"};
         String[] minutesNumberToWord = {"zero", "one", "two", "three", "four", "five",
                 "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen",
                 "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
-        int hours = Integer.parseInt(time.substring(0,1));
-        int minutes = Integer.parseInt(time.substring(2,3));
+        String[] preposition = {"o'clock", "quarter", "past", "half", "to", "minute", "minutes"};
+        int hours = Integer.parseInt(time.substring(0,2));
+        int minutes = Integer.parseInt(time.substring(3,5));
 
-        hours = hours > 12 ? hours / 2 : hours;
+        hours = hours >= 12 ? hours -= 12 : hours;
 
-        return "";
+        StringBuilder timeString = new StringBuilder();
+        switch (minutes) {
+            case 0:
+                timeString.append(hoursNumberToWord[hours]).append(" ").append(preposition[0]);
+                break;
+            case 15:
+                timeString.append(preposition[1]).append(" ").append(preposition[2]).append(" ").append(hoursNumberToWord[hours]);
+                break;
+            case 30:
+                timeString.append(preposition[3]).append(" ").append(preposition[2]).append(" ").append(hoursNumberToWord[hours]);
+                break;
+            case 45:
+                timeString.append(preposition[1]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                break;
+            default:
+                if (minutes == 1) {
+                    timeString.append(minutesNumberToWord[minutes]).append(" ").append(preposition[5]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                } else if (minutes <= 20) {
+                    timeString.append(minutesNumberToWord[minutes]).append(" ").append(preposition[6]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                } else if (minutes < 30) {
+                    timeString.append(minutesNumberToWord[minutes / 10 * 10]).append("-").append(minutesNumberToWord[minutes % 10]).append(" ").append(preposition[5]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                } else if (minutes >= 40){
+                    minutes -= -60;
+                    timeString.append(minutesNumberToWord[minutes]).append(" ").append(preposition[6]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                } else {
+                    minutes -= -60;
+                    timeString.append(minutesNumberToWord[minutes]).append(" ").append(preposition[6]).append(" ").append(preposition[4]).append(" ").append(hoursNumberToWord[hours]);
+                }
+
+        }
+
+        String timeIs = timeString.toString();
+        return timeIs;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inputTime = sc.nextLine();
-        toHumanTime(inputTime);
+        System.out.println(toHumanTime(inputTime));
 
 
 
