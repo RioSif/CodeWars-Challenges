9a880c84075fbdd44b1efacf98ad5bb9217f4936
RioSif <riosif@gmail.com>
2021-05-07 20:51:12 +0300

    :white_check_mark:

--- 6 kyu/Sum of Digits - Digital Root/DRoot.java
+++ 6 kyu/Sum of Digits - Digital Root/DRoot.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class DRoot {
+    public static int digital_root(int n) {
+        int sum = 0;
+        while (n > 10) {
+            sum += n % 10;
+            n /= 10;
+            sum = digital_root(sum);
+        }
+
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(digital_root(num));
+    }
 }
+
