9505d0258ee0d59afa4328f38f85da35e5d7164c
RioSif <riosif@gmail.com>
2021-05-07 12:17:17 +0300

    :white_check_mark:

--- 6 kyu/Multiples of 3 or 5/Multiples of 3 or 5/Solution.java
+++ 6 kyu/Multiples of 3 or 5/Multiples of 3 or 5/Solution.java
@@ -4,14 +4,12 @@ public class Solution {
 
     public static int solution(int number) {
         //TODO: Code stuff here
-        int sum3 = 0, sum5 = 0, minus15 = 0;
 
-        sum3 = (number - 1 - ((number - 1) % 3)) / 3;
-        sum5 = (number - 1 - ((number - 1) % 5)) / 5;
-        minus15 = (number - 1 - ((number - 1) % 15)) / 15;
-        int c= 3 * sum3 * (sum3 + 1) / 2 + 5 * sum5 * (sum5 + 1) / 2 - 15 * minus15 * (minus15 + 1) / 2;
+        int sum3 = (number - 1 - ((number - 1) % 3)) / 3;
+        int sum5 = (number - 1 - ((number - 1) % 5)) / 5;
+        int minus15 = (number - 1 - ((number - 1) % 15)) / 15;
 
-        return c;
+        return 3 * sum3 * (sum3 + 1) / 2 + 5 * sum5 * (sum5 + 1) / 2 - 15 * minus15 * (minus15 + 1) / 2;
     }
 
     public static void main(String[] args) {
