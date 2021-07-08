f310d258675a0bea0a6be8b6b8e6efd46e482e9f
RioSif <riosif@gmail.com>
2021-07-09 00:30:26 +0300

    ✅

--- 6 kyu/Playing with digits/DigPow.java
+++ 6 kyu/Playing with digits/DigPow.java
@@ -6,24 +6,33 @@ public class DigPow {
     public static long digPow(int n, int p) {
         // your code
        // int Digits;
+        long sum = 0;
+        int mynumber = n;
         ArrayList<Integer> list = new ArrayList<Integer>();
         while(n > 0) {
-
-            list.add(n/10);
+            list.add(n % 10);
             n /= 10;
         }
-        for(int i = 0; i > list.size(); i++){
-            System.out.println(list.get(i));
+
+        for(int i = list.size() - 1;i >= 0; i--) {
+            sum += Math.pow(list.get(i),p);
+            p += 1;
         }
-        return -1;
+
+        if(sum % mynumber == 0) {
+            return sum / mynumber;
+        } else {
+            return -1;
+        }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Give number:");
         int number = sc.nextInt();
         System.out.println("Give p");
         int p = sc.nextInt();
-        digPow(number, p);
+        System.out.println(digPow(number, p));
     }
 }
 
